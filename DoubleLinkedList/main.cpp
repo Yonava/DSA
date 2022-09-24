c380e97b3961c33812c2651e684ac9d9211bab87
yonava <yonavossandreae@gmail.com>
2022-09-24 19:06:48 -0400

    enhanced :))

--- DoubleLinkedList/main.cpp
+++ DoubleLinkedList/main.cpp
@@ -3,20 +3,67 @@
 
 using std::string;
 
-int main() {
+struct Node {
+	Node* prev;
+	Node* next;
+	int data;
+};
 
-	struct Node {
-		Node* prev;
-		Node* next;
-		int data;
-	};
+void PrintForward(Node* head);
+void PrintReverse(Node* tail);
+
+int main() {
 
 	Node* head;
 	Node* tail;
 	Node* n;
 
 	n = new Node;
-	
-	n->data = 5;
+	n->data = 1;
+	n->prev = NULL;
+	head = n;
+	tail = n;
 
+	n = new Node;
+	n->data = 2;
+	n->prev = tail;
+	tail->next = n;
+	tail = n;
+
+	n = new Node;
+	n->data = 3;
+	n->prev = tail;
+	tail->next = n;
+	tail = n;
+
+	n = new Node;
+	n->data = 4;
+	n->prev = tail;
+	tail->next = n;
+	tail = n;
+
+	tail->next = NULL;	
+
+	// PrintForward(head);
+	PrintReverse(tail);
+	
+	return 0;
+}
+
+void PrintForward(Node* head) {
+	Node* temp = head;
+
+	while(temp != NULL) {
+		std::cout << temp->data << std::endl;
+		temp = temp->next;
+	}
+}
+
+void PrintReverse(Node* tail) {
+	Node* temp = tail;
+
+	while(temp != NULL) {
+		std::cout << temp->data << std::endl;
+		temp = temp->prev;
+	}
 }
