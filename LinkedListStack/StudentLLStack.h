164468db7b5b2441f963f9f828cddc0e724ddccf
yonava <yonavossandreae@gmail.com>
2022-10-01 23:10:08 -0400

    linked list stack begun

---
+++ LinkedListStack/StudentLLStack.h
@@ -0,0 +1,49 @@
+/*
+	Authored By Yona Voss-Andreae,
+	ID #2073993 - yona.voss-andreae@snhu.edu,
+	CS217 Assignment 4, Array-Based Stack
+*/
+
+#pragma once
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct Student
+{
+	string name;
+	string id;
+	string phoneNum;
+	string address;
+};
+
+struct Node {
+	Node *next;
+	Student *student;
+};
+
+class StudentLLStack {
+	private:
+		int itemsInList = 0;
+		Node *topItem;
+		Node *beforeTop;
+	public:
+		// constructor takes in first item to setup LL
+		StudentLLStack(Student *s);
+
+		// adds item to the stack, returns true if successful
+		bool push(Student *s);
+
+		// removes last item from stack, returns true if successful
+		bool pop();
+
+		// shows user the top item in the stack
+		string peek();
+
+		// returns number of items inside the stack
+		int size();
+		
+		// returns true if stack is empty, otherwise false
+		bool empty();
+};
---
+++ LinkedListStack/main.cpp
@@ -0,0 +1,85 @@
+#include "StudentLLStack.h"
+
+int options()
+{
+
+	// UI that allows users to select from a menu of choices
+	int choice;
+	cout << "------------------" << endl;
+	cout << "Select The Following" << endl;
+	cout << "0 - Push Item" << endl;
+	cout << "1 - Pop Item" << endl;
+	cout << "2 - Peek" << endl;
+	cout << "3 - Length of Stack" << endl;
+	cout << "4 - Reset Stack" << endl;
+	cout << "5 - Is Empty?" << endl;
+	cout << "6 - Exit Program" << endl;
+	cin >> choice;
+
+	return choice;
+}
+
+void createStudent(Student *s)
+{
+	cout << "Student Name: ";
+	cin >> s->name;
+	cout << "Enter Student ID: ";
+	cin >> s->id;
+	cout << "Phone Number: ";
+	cin >> s->phoneNum;
+	cout << "Address: ";
+	cin >> s->address;
+}
+
+int main()
+{
+
+	// instanciates a studentidarray object
+	StudentLLStack* x = new StudentLLStack;
+	Student s;
+
+	int selectedOption;
+	bool running = true;
+
+	while (running)
+	{
+		selectedOption = options();
+		cout << "------------------" << endl;
+		switch (selectedOption)
+		{
+			case 0:
+				createStudent(s);
+				cout << x->push(s) << endl;
+				break;
+			case 1:
+				cout << x->pop() << endl;
+				break;
+			case 2:
+				x->peek();
+				break;
+			case 3:
+				cout << "Your Stack Has A Length Of: " << x->size() << endl;
+				break;
+			case 4:
+				cout << "Stack Reset." << endl;
+				x->resetStack();
+				break;
+			case 5:
+				if (x->empty()) {
+					cout << "Stack is empty" << endl;
+				} else {
+					cout << "Stack is not empty" << endl;
+				}
+				break;
+			case 6:
+				cout << "Bye!" << endl;
+				running = false;
+				break;
+			default:
+				cout << "Input Unrecognized" << endl;
+				break;
+		}
+	}
+
+	return 0;
+}
