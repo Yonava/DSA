33eb182351087468a3855378a203b8a8b06991a1
Yonava <yonavossandreae@gmail.com>
2022-11-11 23:10:29 -0500

    added all 2d shapes

---
+++ Assignment7/Shapes.cpp
@@ -0,0 +1,91 @@
+#include "Shapes.h"
+
+Square::Square(float sideLength) {
+  s = sideLength;
+}
+
+float Square::Area() {
+  return s * s;
+}
+
+string Square::GetName2D() {
+  return "Square";
+}
+
+Rectangle::Rectangle(float width, float length) {
+  w = width;
+  l = length;
+}
+
+float Rectangle::Area() {
+  return w * l;
+}
+
+string Rectangle::GetName2D() {
+  return "Rectangle";
+}
+
+Triangle::Triangle(float base, float height) {
+  b = base;
+  h = height;
+}
+
+float Triangle::Area() {
+  return (b * h) / 2;
+}
+
+string Triangle::GetName2D() {
+  return "Triangle";
+}
+
+Circle::Circle(float radius) {
+  r = radius;
+}
+
+float Circle::Area() {
+  return r * r * PI;
+}
+
+string Circle::GetName2D() {
+  return "Circle";
+}
+
+Ellipse::Ellipse(float majorAxis, float minorAxis) {
+  maj = majorAxis;
+  min = minorAxis;
+}
+
+float Ellipse::Area() {
+  return maj * min * PI;
+}
+
+string Ellipse::GetName2D() {
+  return "Ellipse";
+}
+
+Trapezoid::Trapezoid(float sideA, float sideB, float height) {
+  a = sideA;
+  b = sideB;
+  h = height;
+}
+
+float Trapezoid::Area() {
+  return .5 * (a + b) * h;
+}
+
+string Trapezoid::GetName2D() {
+  return "Trapezoid";
+}
+
+Sector::Sector(float radius, float angleInDegrees) {
+  r = radius;
+  a = angleInDegrees;
+}
+
+float Sector::Area() {
+  return  .5 * r * r * a;
+}
+
+string Sector::GetName2D() {
+  return "Sector";
+}
--- Assignment7/Shapes.h
+++ Assignment7/Shapes.h
@@ -8,6 +8,7 @@ using std::string;
 class Shape
 {
 public:
+  const float PI = 3.14159f; 
   virtual void Scale(float scaleFactor) = 0;
   virtual void Display() const = 0;
   virtual ~Shape() = 0;
@@ -22,6 +23,75 @@ public:
   bool operator>(const Shape2D &rhs) const;
   bool operator<(const Shape2D &rhs) const;
   bool operator==(const Shape2D &rhs) const;
+};
+
+class Square : virtual public Shape2D
+{
+  float s;
+  public:
+    Square(float sideLength = 0);
+    string GetName2D();
+    float Area();
+};
+
+class Rectangle : virtual public Shape2D
+{
+  float l;
+  float w;
+  public:
+    Rectangle(float width = 0, float length = 0);
+    string GetName2D();
+    float Area();
+};
+
+class Triangle : virtual public Shape2D
+{
+  float b;
+  float h;
+  public:
+    Triangle(float base = 0, float height = 0);
+    string GetName2D();
+    float Area();
+};
+
+class Circle : virtual public Shape2D
+{
+  float r;
+  public:
+    Circle(float radius = 0);
+    string GetName2D();
+    float Area();
+};
+
+class Ellipse : virtual public Shape2D
+{
+  float maj;
+  float min;
+  public:
+    Ellipse(float majorAxis = 0, float minorAxis = 0);
+    string GetName2D();
+    float Area();
+};
+
+class Trapezoid : virtual public Shape2D
+{
+  float a;
+  float b;
+  float h;
+  public:
+    Trapezoid(float sideA = 0, float sideB = 0, float height = 0);
+    string GetName2D();
+    float Area();
+};
+
+class Sector : virtual public Shape2D
+{
+  float r;
+  float a;
+  public:
+    Sector(float radius = 0, float angleInDegrees = 0);
+    string GetName2D();
+    float Area();
 };
 
 class Shape3D : virtual public Shape
