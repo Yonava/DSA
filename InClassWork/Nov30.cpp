897c8de6e92cc9edf359e58f44c163cdce6378da
Yonava <yonavossandreae@gmail.com>
2022-11-30 13:14:58 -0500

    added main func to classwork

--- InClassWork/Nov30.cpp
+++ InClassWork/Nov30.cpp
@@ -1,6 +1,8 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
+
 
 template <typename T>
 T array_max(T a[], size_t n)
@@ -10,4 +12,11 @@ T array_max(T a[], size_t n)
     if (a[i] > max)
       max = a[i];
   return max;
+}
+
+int main() {
+  int const arr_size = 6;
+  int arr[arr_size] = {1, 2, 3, 6, 4, 5};
+  int max = array_max(arr, arr_size);
+  cout << max << endl;
 }
